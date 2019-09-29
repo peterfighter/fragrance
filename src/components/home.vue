1c73610917e8e2553b4e635b6bb38191f1263b10
FlyingWorm <peterfighter@126.com>
2019-09-29 13:45:39 +0800

    首次提交

---
+++ src/components/home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="home">
+    <div class="home-header">
+      conceal fragrance
+    </div>
+    <div class="lump-gap"></div>
+    <div class="text-content">
+      <div class="content-height">I am the daughter of the</div>
+      <div class="content-height">elements With whom Winter conceived; </div>
+      <div class="content-height">To whom Spring gave birth;</div>
+      <div class="content-height">I was Reared in the lap of Summer and </div>
+      <div class="content-height">I Slept in the bed of Autumn.</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .home-header{
+        height: 150px;
+      text-align: center;
+      line-height: 150px;
+      font-family: fantasy;
+      font-size: 46px;
+      background-color: beige;
+      min-width: 600px;
+  }
+  .lump-gap{
+    height: 20px;
+  }
+  .text-content{
+    text-align: center;
+  }
+  .content-height{
+    height: 40px;
+    line-height: 40px;
+  }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import home from '@/components/home'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: home
+    },
+    {
+      path: '/HelloWorld',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
+})
